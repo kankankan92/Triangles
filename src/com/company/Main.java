45f5ef463ac38bc8c01ac3442c575caa6ae605ef
Anastasia <kankankan92@mail.ru>
2018-10-23 12:20:08 +0300

    merged master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(2, 1);
-        Point point2 = new Point(1, -2);
+        Point point1 = new Point(5, 2);
+        Point point2 = new Point(3, -4);
         Point point3 = new Point(-1, 0);
-        Vector vector1 = new Vector(point1, point2);
-        Vector vector2 = new Vector(point1, point3);
-        System.out.println(vector1.cosAngle(vector1.scalarProduct(vector1,vector2), vector1.getLenght()*vector2.getLenght()));
+        Point point4 = new Point(2, 1);
+        Point point5 = new Point(1, -2);
+        Point point6 = new Point(-1, 0);
+        Triangle triangle1 = new Triangle(point1, point2, point3);
+        Triangle triangle2 = new Triangle(point4, point5, point6);
+        System.out.println(triangle1.angles[0]+","+ triangle1.angles[1]+","+ triangle1.angles[2]);
+        triangle1.sortAngles(triangle1.angles);
+        System.out.println(triangle1.angles[0]+","+ triangle1.angles[1]+","+ triangle1.angles[2]);
+        System.out.println(Triangle.similarityTriangles(triangle1, triangle2));
     }
 }

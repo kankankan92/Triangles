b751bfbbb716fad49b2ef86be1b4e5a792ddf652
Anastasia <kankankan92@mail.ru>
2018-10-23 12:39:53 +0300

    fixed commit after after cherry pick

--- src/com/company/Vector.java
+++ src/com/company/Vector.java
@@ -4,34 +4,29 @@ public class Vector {
 
     private final double x;
     private final double y;
-    private double lenght;
+    private double length;
 
     Vector(Point start, Point end){
-        this.x = start.getX()- end.getX();
-        this.y = start.getY()- end.getY();
-        this.lenght = Math.sqrt(getX()*getX()+getY()*getY());
+        x = start.getX()- end.getX();
+        y = start.getY()- end.getY();
+        length = Math.sqrt(getX()*getX()+getY()*getY());
     }
 
-    public double scalarProduct(Vector vector1, Vector vector2){
+    public static double scalarProduct(Vector vector1, Vector vector2){
         double scalarProduct = vector1.getX()*vector2.getX()+vector1.getY()*vector2.getY();
         return scalarProduct;
     }
 
-    public double cosAngle(double scalarProduct, double lenght){
-        double cosAngle = scalarProduct/lenght;
-        return cosAngle;
-    }
-
-    public double getX() {
+    private double getX() {
         return x;
     }
 
-    public double getY() {
+    private double getY() {
         return y;
         }
 
-    public double getLenght() {
-        return lenght;
+    public double getLength() {
+        return length;
     }
 
 }
